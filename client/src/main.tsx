23c606c344b99061bb5a3a2624a7dbc30a1a1b22
sebby-omarchy <sebgorgone@gmail.com>
2026-04-20 22:47:28 -0400

    routing scaffold

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Routes, Route, BrowserRouter } from "react-router-dom"
+import { useMemo } from 'react'
+import { Routes, Route, BrowserRouter, useNavigate } from "react-router-dom"
+import { useParams } from 'react-router-dom'
 import './index.css'
 import App from './routes/App.tsx'
+
+function RoomConnectionRoute() {
+  const { roomCode } = useParams()
+
+  const normalizedRoomCode = useMemo(() => {
+    return (roomCode ?? '').toUpperCase()
+  }, [roomCode])
+
+  const hasValidCharacters = /^[A-Z0-9]+$/.test(normalizedRoomCode)
+
+  if (!normalizedRoomCode || !hasValidCharacters) {
+    return useNavigate('/')
+  }
+
+  return <>connecting</>>
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -10,6 +28,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
 
         <Route path='/' element={<App/>} />
+        <Route path='/:roomCode' element={<RoomConnectionRoute />} />
 
       </Routes>
     </BrowserRouter>
