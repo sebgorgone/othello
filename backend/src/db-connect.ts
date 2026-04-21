ed0022b4b1c699f6b9134aeb02880040ae66a262
dinomolasses <sebgorgone>
2026-04-21 11:17:55 -0400

    okay this cant possibly break

--- backend/src/db-connect.ts
+++ backend/src/db-connect.ts
@@ -1,4 +1,12 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({path: path.resolve(__dirname, "../.env")});
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
