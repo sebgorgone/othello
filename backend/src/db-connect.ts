d4502c0beaf25e283ececc38f2f1c52e4d45b8a4
dinomolasses <sebgorgone>
2026-04-21 11:23:05 -0400

    oh my god/

--- backend/src/db-connect.ts
+++ backend/src/db-connect.ts
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 
   waitForConnections: true,
