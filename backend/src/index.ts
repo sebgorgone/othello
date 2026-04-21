82e5fba24c9f4da40a07c5a54c73f549e07c59ea
sebby-omarchy <sebgorgone@gmail.com>
2026-04-21 15:07:15 -0400

    cors was the problem

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config({path: path.resolve(__dirname, "../.env")});
 const app = express();
 app.use(cors({
-  origin: ['*']
+  origin: '*'
 }));
 
 type Room = {
