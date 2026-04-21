1fb0ba93c4d2793d042913ee1fc5a087284022d8
sebby-omarchy <sebgorgone@gmail.com>
2026-04-21 15:42:43 -0400

    whats even the point that should have worked

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { useMemo } from 'react'
-import { Routes, Route, BrowserRouter, useNavigate } from "react-router-dom"
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom"
 import { useParams } from 'react-router-dom'
 import './index.css';
 import App from './routes/App.tsx';
@@ -17,10 +17,11 @@ function RoomConnectionRoute() {
   const hasValidCharacters = /^[A-Z0-9]+$/.test(normalizedRoomCode)
 
   if (!normalizedRoomCode || !hasValidCharacters) {
-    return {useNavigate('/'); console.log('room code failed sanitization')}
+    console.log('invalid code redirecting');
+    return <Navigate to='/' replace />
   }
 
-  return <OthelloRoom code={roomCode}/> 
+  return <OthelloRoom code={normalizedRoomCode}/> 
 }
 
 createRoot(document.getElementById('root')!).render(
